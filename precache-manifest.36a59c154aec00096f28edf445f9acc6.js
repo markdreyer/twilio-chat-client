c21e4f6533c7fbb5998c3eeda2adaa9dc1b0ae2a
Mark Dreyer <dreyguy@gmail.com>
2020-06-24 19:41:56 -0500

    Initial commit of sms.risasoft.com

---
+++ precache-manifest.36a59c154aec00096f28edf445f9acc6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7459097e9da93c14008daee3581053",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56bba81788cde39316b0",
+    "url": "/static/css/2.daa8cbb1.chunk.css"
+  },
+  {
+    "revision": "8042faebfc280816ad7b",
+    "url": "/static/css/main.fbd04e45.chunk.css"
+  },
+  {
+    "revision": "56bba81788cde39316b0",
+    "url": "/static/js/2.62b7a3cb.chunk.js"
+  },
+  {
+    "revision": "ca30ab9dca25fd80f758bed087990ccd",
+    "url": "/static/js/2.62b7a3cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8042faebfc280816ad7b",
+    "url": "/static/js/main.cb3a68fc.chunk.js"
+  },
+  {
+    "revision": "68edeee5adcc0e37d57b",
+    "url": "/static/js/runtime-main.a546face.js"
+  },
+  {
+    "revision": "4c36e3279ce65a779851df90fcc07e94",
+    "url": "/static/media/twilio-mark-red.4c36e327.svg"
+  }
+]);
